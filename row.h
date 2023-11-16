5d320f90f7576b83930ad7f4d72573084a689067
zhouchao03 <zhouchao03@inspur.com>
2023-11-16 08:32:01 +0800

    feat: remove row module for unused.

--- row.h
+++
@@ -1,7 +0,0 @@
-#include "data.h"
-
-// Merge two select result
-SelectResult *merge(SelectResult *first_result, SelectResult *sec_result);
-
-// Reduce two select result
-SelectResult *reduce(SelectResult *first_result, SelectResult *sec_result);
