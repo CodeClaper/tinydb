d3065d7fbccaa10e50dbb02a9a9dc6dcb2697b5a
zhouchao03 <zhouchao03@inspur.com>
2023-11-20 22:01:33 +0800

    raf: change main.c to db.c

--- src/session.h
+++ src/session.h
@@ -1,15 +1,15 @@
 #include "data.h"
 
-/*init session*/
+/* Initialise session. */
 void init_session();
 
-/*Generate new session.*/
+/* Generate new session. */
 Session *new_session(int client);
 
-/*Set session to pthread_key_t.*/
+/* Set session to pthread_key_t. */
 void set_session(void *session);
 
-/*Get session from pthread_key_t.*/
+/* Get session from pthread_key_t. */
 Session *get_session();
 
 /* Socket send
@@ -22,9 +22,9 @@ bool db_send(const char *buff);
  * */
 bool db_send_over();
 
-/*destroy session, free the session memory. */
+/* Destroy session, free the session memory. */
 void destroy_session();
 
-/*Delete pthread_key_t*/
+/* Delete pthread_key_t*/
 void end_session();
 
