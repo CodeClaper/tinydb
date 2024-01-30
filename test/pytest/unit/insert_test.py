2b341fda83de3c96b377c1f68cdf5ec7474538d2
zhouchao03 <zhouchao03@inspur.com>
2024-01-30 16:04:10 +0800

    test: test for in or like predicate.

--- test/pytest/unit/insert_test.py
+++ test/pytest/unit/insert_test.py
@@ -6,7 +6,9 @@ client = TinyDbClient("127.0.0.1", 4083)
 
 ## test insert one.
 def test_insert_one():
-    ret = client.execute("insert into S values('S001', 'zhangsan', 10, 'beijing');")
+    ret = client.execute("insert into S values('S001', 'lili', 18, 'beijing');")
+    assert ret["success"] == True
+    ret = client.execute("insert into S values('S002', 'zhanglan', 24, 'shanghai');")
     assert ret["success"] == True
 
 ## test dupliacate key.
--- test/pytest/unit/select_test.py
+++ test/pytest/unit/select_test.py
@@ -10,19 +10,19 @@ client = TinyDbClient("127.0.0.1", 4083)
 def test_select_all():
     ret = client.execute("select * from S");
     assert ret["success"] == True
-    assert ret["rows"] == 1
+    assert ret["rows"] == 2
 
 ## test select one item
 def test_select_one_item():
     ret = client.execute("select id from S");
     assert ret["success"] == True
-    assert ret["data"] == { "id": "S001" }
+    assert ret["data"] == [{ "id": "S001" }, {"id": "S002"}]
 
 ## test select multiple items
 def test_select_items():
     ret = client.execute("select id, name from S");
     assert ret["success"] == True
-    assert ret["data"] == { "id": "S001", "name": "zhangsan" }
+    assert ret["data"] == [{ "id": "S001", "name": "lili" }, { "id": "S002", "name": "zhanglan" }]
 
 
 ## test select not exists items.
@@ -38,7 +38,7 @@ def test_select_simple_condition():
 
 ## test select complex condition
 def test_select_complex_condition1():
-    ret = client.execute("select id from S where age = 10 and address = 'beijing'")
+    ret = client.execute("select id from S where age = 18 and address = 'beijing'")
     assert ret["success"] == True
     assert ret["data"] == { "id": "S001" }
 
@@ -47,6 +47,55 @@ def test_select_complex_condition2():
     ret = client.execute("select id from S where age > 100 and address = 'beijing'")
     assert ret["success"] == True
     assert ret["data"] == []
+
+## test select complex condition
+def test_select_complex_condition3():
+    ret = client.execute("select id from S where age < 20 and age > 10 or address = 'shanghai'")
+    assert ret["success"] == True
+    assert ret["rows"] == 2
+
+## test select complex condition
+def test_select_complex_condition4():
+    ret = client.execute("select id from S where (age < 20 and age > 10 or address = 'beijing')")
+    assert ret["success"] == True
+    assert ret["rows"] == 1
+
+## test select like predicate.
+def test_select_like_predicate1():
+    ret = client.execute("select id from S where address like 'bei%'")
+    assert ret["success"] == True
+    assert ret["rows"] == 1
+
+## test select like predicate.
+def test_select_like_predicate2():
+    ret = client.execute("select id from S where address like '%hai'")
+    assert ret["success"] == True
+    assert ret["rows"] == 1
+
+## test select like predicate.
+def test_select_like_predicate3():
+    ret = client.execute("select id from S where address like '%ng%'")
+    assert ret["success"] == True
+    assert ret["rows"] == 2
+
+
+## test select in predicate.
+def test_select_in_predicate1():
+    ret = client.execute("select id from S where id in ('S001')")
+    assert ret["success"] == True
+    assert ret["rows"] == 1
+
+## test select in predicate.
+def test_select_in_predicate2():
+    ret = client.execute("select id from S where id in ('S001', 'S002')")
+    assert ret["success"] == True
+    assert ret["rows"] == 2
+
+## test select in predicate.
+def test_select_in_predicate3():
+    ret = client.execute("select id from S where id in ('S004', 'S005')")
+    assert ret["success"] == True
+    assert ret["rows"] == 0
 
 ## test select condition field not exist.
 def test_select_not_exist_condition_filed():
