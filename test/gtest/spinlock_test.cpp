ef1465951ec7d74990763c81239e0161795eb7d2
zhouchao03 <zhouchao03@inspur.com>
2024-03-05 09:00:18 +0800

    test: intro gtest.

--- test/gtest/spinlock_test.cpp
+++ test/gtest/spinlock_test.cpp
@@ -1,5 +1,11 @@
 #include "gtest/gtest.h"
 
-TEST(first, ok) {
+extern "C" {
+#include "include/spinlock.h"
+}
+
+static s_lock lock = SPIN_UN_LOCKED_STATUS;
+
+TEST(spinlock, ok) {
     ASSERT_EQ(1, 1);
 }
--- test/pytest/unit/batch_insert_demo.py
+++ test/pytest/unit/batch_insert_demo.py
@@ -8,6 +8,6 @@ classList = ["C0001", "C0002", "C0003", "C0004"]
 
 ## test insert one.
 def test_insert1():
-    for i in range(1, 20000):
+    for i in range(1, 10000):
         client.execute("insert into Student values('{}', '{}', {}, '{}', ref(id='{}'));".format(i, nameList[i % 4], 10, 'M', classList[i % 4]))
 
