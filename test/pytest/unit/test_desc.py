38e881c4b2f09e65543da2e9897d7dd2334d1538
zhouchao <jerryzhou.zc@gmail.com>
2024-06-09 23:11:11 +0800

    test: add test about desc.

---
+++ test/pytest/unit/test_desc.py
@@ -0,0 +1,31 @@
+## test_desc.py
+from support.tinydb_cli import TinyDbClient
+
+client = TinyDbClient("127.0.0.1", 4083)    
+
+## Firstly create table.
+def test_create_table():
+    sql = "CREATE TABLE Student(id varchar(32) primary key comment 'Student Id', name varchar(32) not null comment 'Student full name', \
+           age int comment 'Student age', address varchar(64) default 'unknown' comment 'Student address');"
+    ret = client.execute(sql)
+    assert ret["success"] == True
+
+## Desc table after creating table.
+def test_desc_table():
+    sql = "DESC Student;"
+    ret = client.execute(sql)
+    assert ret["success"] == True
+    assert ret["data"] == [
+        { "field": "id", "type": "varchar", "length": 32, "array": False, "key": "primary", "comment": "Student Id" },
+        { "field": "name", "type": "varchar", "length": 32, "array": False, "key": None, "comment": "Student full name" },
+        { "field": "age", "type": "int", "length": 4, "array": False, "key": None, "comment": "Student age" },
+        { "field": "address", "type": "varchar", "length": 64, "array": False, "key": None, "comment": "Student address", "default": "unknown" }
+    ]
+
+## Drop table Student. 
+def test_drop_table():
+    sql = "DROP TABLE Student;"
+    ret = client.execute(sql)
+    assert ret["success"] == True
+
+
