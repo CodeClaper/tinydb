b3882d71969ae68127283db7e2b00010509f765e
zhouchao <jerryzhou.zc@gmail.com>
2024-06-28 11:44:03 +0800

    test: add some test.

--- test/pytest/unit/test_data_type.py
+++ test/pytest/unit/test_data_type.py
@@ -1,11 +1,14 @@
 from support.tinydb_cli import TinyDbClient
+from support.asserts import assert_all
 
 client = TinyDbClient("127.0.0.1", 4083)    
 
 ## test create table
 def test_create_table():
-    ret = client.execute("create table A(id varchar(8), name varchar(16), age int, class int, sex char,  score float, phone varchar(11), address varchar(48), primary key(id))")
-    assert ret["success"] == True
+    sql = "create table A(id varchar(8), name varchar(16), age int, class int, sex char, score float, phone varchar(11), address varchar(48), primary key(id));\n"\
+          "create table F(id int, diamend double);"
+    ret = client.execute(sql)
+    assert_all(ret)
 
 ## test overflow varchar.
 def test_overflow_varchar():
@@ -28,7 +31,6 @@ def test_overflow_char():
     assert ret["success"] == False
     assert ret["message"] == "Try to convert value 'MAN' to char value type fail."
 
-
 ## test overflow char.
 def test_overflow_int():
     sql = "INSERT INTO A values('S001', 'jack', 1314232272847489294756329290476549394673, 2, 'M', 3.23, '13001542702', 'beijing');"
@@ -44,9 +46,30 @@ def test_invalid_number():
     assert ret["success"] == False
     assert ret["message"] == "Sql syntax error near [erwswq]."
 
-## test for drop table.
-def test_drop_table():
-    sql = "DROP TABLE A;"
+## test float overflow.
+def test_overflow_float():
+    sql = "INSERT INTO A VALUES('S001', 'mary', 10, 4, 'F', 9999992312313131238173472748273472874827348738478274832784782748273847824782748728478294782783472893213287382738274872837827387283728892832738728478273872837827382.231872463478718273817483784723847823, '13023237472', 'beijing')";
+    ret = client.execute(sql)
+    assert ret["success"] == False
+    assert ret["message"] == "Value is float overflow for column 'score'."
+
+## test insert big double value.
+def test_insert_big_double():
+    sql = "INSERT INTO F VALUES(1, 9999992312313131238173472748273472874827348738478274832784782748273847824782748728478294782783472893213287382738274872837827387283728892832738728478273872837827382.231872463478718273817483784723847823)";
     ret = client.execute(sql)
     assert ret["success"] == True
+
+## test query double value.
+def test_query_double_value():
+    sql = "SELECT diamend from F;";
+    ret = client.execute(sql)
+    assert ret["success"] == True
+    assert ret["data"][0]["diamend"] == 9999992312313131238173472748273472874827348738478274832784782748273847824782748728478294782783472893213287382738274872837827387283728892832738728478273872837827382.231872463478718273817483784723847823
+
+## test for drop table.
+def test_drop_table():
+    sql = "DROP TABLE A;\n" \
+          "DROP TABLE F;"
+    ret = client.execute(sql)
+    assert_all(ret)
 
