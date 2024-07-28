20d448b9307f67ed5855d8e0785645fe9359fb21
zhouchao <jerryzhou.zc@gmail.com>
2024-07-28 19:11:30 +0800

    raf: replace TableBuffer with List.

--- src/memory/buffer.c
+++ src/memory/buffer.c
@@ -13,21 +13,39 @@
 #include "asserts.h"
 #include "spinlock.h"
 
-/* Save or update table buffer. */
-static bool save_or_update_table_buffer(Table *table);
 
 /* Table Buffer. */
-static TableBuffer *buffer = NULL;
+static List *buffer_list;
 
-static volatile s_lock lock = SPIN_UN_LOCKED_STATUS;
+/* Lock for table buffer. */
+static volatile s_lock lock;
 
 /* Initialise table buffer. */
 void init_table_buffer() {
-    if (buffer == NULL) {
-        buffer = instance(TableBuffer);
-        buffer->size = 0;
-        buffer->buffer = db_malloc(0, "pointer"); 
-    }
+    spin_lock_init(&lock);
+    buffer_list = create_list(NODE_TABLE_BUFFER_ENTRY);
+}
+
+/* Save or update table buffer. */
+static bool save_or_update_table_buffer(Table *table) {
+
+    assert_not_null(table, "Input table must not be NULL.");
+
+    /* Try to get current transaction. */
+    TransactionHandle *trans = find_transaction();
+    if (!trans) 
+        return false;
+
+    /* Generate TableBufferEntry. */
+    TableBufferEntry *entry = instance(TableBufferEntry);
+    entry->table = table;
+    entry->tid = trans->tid;
+    entry->xid = trans->xid;
+    
+    /* Append to buffer. */
+    append_list(buffer_list, entry);
+
+    return true;
 }
 
 /* Find table in table buffer. */
@@ -40,9 +58,10 @@ Table *find_table_buffer(char *table_name) {
 
     /* Firstly, find in buffer. */
     if (trans) {
-        int32_t i;
-        for (i = 0; i < buffer->size; i++) {
-            TableBufferEntry *entry = buffer->buffer[i];
+
+        ListCell *lc;
+        foreach (lc, buffer_list) {
+            TableBufferEntry *entry = lfirst(lc);
             if (streq(table_name, entry->table->meta_table->table_name) 
                 && entry->xid == trans->xid 
                 && entry->tid == trans->tid) {
@@ -64,55 +83,24 @@ Table *find_table_buffer(char *table_name) {
     return cache_table;
 }
 
-/* Save or update table buffer. */
-static bool save_or_update_table_buffer(Table *table) {
-
-    assert_not_null(table, "Input table must not be NULL.");
-
-    /* Try to get current transaction. */
-    TransactionHandle *trans = find_transaction();
-    if (!trans) 
-        return false;
-
-    /* Generate TableBufferEntry. */
-    TableBufferEntry *entry = instance(TableBufferEntry);
-    entry->table = table;
-    entry->tid = trans->tid;
-    entry->xid = trans->xid;
-    
-    /* Append to buffer. */
-    buffer->buffer = db_realloc(buffer->buffer, sizeof(TableBufferEntry *) * (buffer->size + 1));
-    buffer->buffer[buffer->size] = entry;
-    buffer->size++;
-
-    return true;
-}
 
 /* Remove all of whole current transaction releated TableBufferEntries. */
 bool remove_table_buffer() {
     /* Try to get current transaction. */
     TransactionHandle *trans = find_transaction();
-    if (trans == NULL) 
+    if (is_null(trans))
         return false;
 
     spin_lock_acquire(&lock);
 
-    int32_t i, j;
-    for (i = 0; i < buffer->size; i++) {
-        TableBufferEntry *entry = buffer->buffer[i];
+    uint32_t i;
+    for (i = 0; i < len_list(buffer_list); i++) {
+        TableBufferEntry *entry = lfirst(list_nth_cell(buffer_list, i));
         if (entry->xid == trans->xid && entry->tid == trans->tid) {
-            /* Right move forwar to cover left space. */
-            for (j = i; j < buffer->size - 1; j++) {
-                memcpy(buffer->buffer + j, buffer->buffer + j + 1, sizeof(TableBufferEntry *));
-            }
-            memset(buffer->buffer + buffer->size - 1, 0, sizeof(TableBufferEntry *));
-            buffer->size--;
-            buffer->buffer = db_realloc(buffer->buffer, sizeof(TableBufferEntry *) * buffer->size);
             free_table_buffer_entry(entry);
-
-            /* Restart loop check. */
-            i = 0;
-        }  
+            list_delete(buffer_list, entry);
+            i = 0; // Restart over.
+        }
     }
     
     spin_lock_release(&lock);
@@ -124,21 +112,13 @@ void clear_table_buffer(char *table_name) {
 
     spin_lock_acquire(&lock);
 
-    int32_t i, j;
-    for (i = 0; i < buffer->size; i++) {
-        TableBufferEntry *entry = buffer->buffer[i];
+    uint32_t i;
+    for (i = 0; i < len_list(buffer_list); i++) {
+        TableBufferEntry *entry = lfirst(list_nth_cell(buffer_list, i));
         if (streq(table_name, entry->table->meta_table->table_name)) {
-            /* Right move forwar to cover left space. */
-            for (j = i; j < buffer->size - 1; j++) {
-                memcpy(buffer->buffer + j, buffer->buffer + j + 1, sizeof(TableBufferEntry *));
-            }
-            memset(buffer->buffer + buffer->size - 1, 0, sizeof(TableBufferEntry *));
-            buffer->size--;
-            buffer->buffer = db_realloc(buffer->buffer, sizeof(TableBufferEntry *) * buffer->size);
             free_table_buffer_entry(entry);
-
-            /* Restart loop check. */
-            i = 0;
+            list_delete(buffer_list, entry);
+            i = 0; // Restart over.
         }  
     }
     
