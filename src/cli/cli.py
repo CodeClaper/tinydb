898ffe7b6f6e7ce93425dc14c6efa7e4da13f386
zhouchao <jerryzhou.zc@gmail.com>
2024-09-13 09:18:11 +0800

    feat: add python cli.

---
+++ src/cli/cli.py
@@ -0,0 +1,82 @@
+# cli.py
+
+import readline
+import os
+import sys
+import connector
+
+client = connector.TinyDbClient('127.0.0.1', 4083)
+
+def completer(text, state):
+    options = ['SELECT', 'UPDATE', 'DELETE', 'INSERT', 'DROP', 'CREATE', 'TABLE', 'FROM', 'WHERE', 
+               'AND', 'OR', 'ALTER', 'DESC', 'DESCRIBE']
+    options = [i for i in options if i.startswith(text.upper())]
+
+    try:
+        return options[state]
+    except:
+        pass
+
+readline.set_completer(completer)
+readline.parse_and_bind('tab: complete')
+readline.parse_and_bind('Control-v: paste')
+
+## exec exit.
+def exit():
+    print('Bye...')
+    sys.exit(0)
+
+## exec clear 
+def clear():
+    os.system("clear")
+
+
+## execute tinydb
+def tinydb(cmd):
+    ret = client.execute(cmd)    
+    print(ret)
+
+## read command.
+def readCmd():
+    try:
+        return input('tinydb > ')
+    except KeyboardInterrupt:
+        exit()
+
+## source file.
+def source(file):
+    try:
+        with open(file) as file:
+            for line in file:
+                sql = line.strip()
+                ret = client.execute(sql)
+                print (ret)
+                if ret["success"] == False:
+                    break
+    except FileNotFoundError:
+        print(f"file {file} not found")
+        pass
+    except KeyboardInterrupt:
+        exit()
+
+
+## Execute command.
+def exec_cmd(cmd):
+    if cmd.upper().startswith("SOURCE "):
+        source(cmd.strip("SOURCE ").strip("source" ).strip())
+    else:
+        match cmd.upper():
+            case 'EXIT':
+                exit()
+            case 'CLEAR':
+                clear()
+            case 'CLS':
+                clear()
+            case _:
+                tinydb(cmd)
+
+
+if __name__ == "__main__":
+    while True:
+        cmd = readCmd()
+        exec_cmd(cmd)
---
+++ src/cli/connector.py
@@ -0,0 +1,33 @@
+#connector.py
+
+import io
+import socket
+import json
+
+class TinyDbClient:
+    def __init__(self, ip, port):
+        # create a socket object.
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((ip, port))
+
+    def execute(self, sql) -> dict:
+        try:
+            self.client.send(sql.encode("utf-8")[:1024])
+            writer = io.StringIO()
+            while True:
+                response = self.client.recv(1024)
+                response = response.decode("utf-8").rstrip("\x00")
+                if response.upper() == "OVER":
+                    break
+                writer.write(response)
+            ret = writer.getvalue()
+            writer.close()
+            return json.loads(ret)
+               
+        except Exception as e:
+            print(f"Error: {e}")
+
+        return {}
+
+    def close(self):
+        self.client.close()
