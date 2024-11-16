019f30de97d4ebde4a5e3a7043105d3dc18cf434
zhouchao <jerryzhou.zc@gmail.com>
2024-11-16 16:04:30 +0800

    doc: change the ASCII art content.

--- include/common/banner.h
+++ include/common/banner.h
@@ -1,10 +1,9 @@
-#define LOG    " ooooooooooooo  o8o                          oooooooooo.   oooooooooo.   \n"\
-               " 88   888   88   88                          g888'    Y8b   888'    Y8b  \n"\
-               "      888      oooo  ooo. .oo.   oooo    ooo  888      888  888     888  \n"\
-               "      888       888   888PPY88b   '88.  .8'   888      888  888oooo888'  \n"\
-               "      888       888   888   888    '88..8'    888      888  888    g88b  \n"\
-               "      888       888   888   888     '888'     888     d88  888    .88P   \n"\
-               "     o888o     o888o o888o o888o     .8'     o888bood8P'   o888bood8P'   \n"\
-               "                                .o..P'                                   \n"\
-               "                               gY8P                                      \n\n"\
-               "Welcome to TinyDb.\nYour TinyDb version is 0.0.1.\n\nCopyright (c) 2024, Inspur.\n"
+#define LOG    " .-----. _             .---. .---.  \n"\
+               " `-. .-':_;            : .  :: .; : \n"\
+               "   : :  .-.,-.,-..-..-.: :: ::   .' \n"\
+               "   : :  : :: ,. :: :; :: :; :: .; : \n"\
+               "   :_;  :_;:_;:_;`._. ;:___.':___.' \n"\
+               "                  .-. :             \n"\
+               "                  `._.'             \n\n"\
+               " Welcome to TinyDb. Your TinyDb version is 0.0.1. \n"\
+               " Copyright (c) 2024, Inspur.com     \n"
