0d085bd8530c6b89ddc64c83ea087fb6fcbdcda2
zhouchao <jerryzhou.zc@gmail.com>
2025-01-07 23:37:28 +0800

    test: add more endback to query.

--- test/pytest/concurrency/test_insert_with_select.py
+++ test/pytest/concurrency/test_insert_with_select.py
@@ -7,23 +7,15 @@ import threading
 import random
 import time
 
+clients = []
 threads = []
 share_resource = {}
 
-client1 = TinyDbClient("127.0.0.1", 4083)    
-client1.login("root", "Zc120130211")
+for _ in range(0, 30):
+    cli = TinyDbClient("127.0.0.1", 4083)    
+    cli.login("root", "Zc120130211")
+    clients.append(cli)
 
-client2 = TinyDbClient("127.0.0.1", 4083)    
-client2.login("root", "Zc120130211")
-
-client3 = TinyDbClient("127.0.0.1", 4083)    
-client3.login("root", "Zc120130211")
-
-client4 = TinyDbClient("127.0.0.1", 4083)    
-client4.login("root", "Zc120130211")
-
-client5 = TinyDbClient("127.0.0.1", 4083)    
-client5.login("root", "Zc120130211")
 
 # thread to insert.
 def thread_insert(cli):
@@ -39,28 +31,25 @@ def thread_insert(cli):
 # thread1 to select
 def thread_select(cli):
     try:
-        for _ in range(1, 60):
+        for _ in range(1, 300):
             sql = "select count(1) from Student;"
             ret = cli.execute(sql)
             assert ret["success"] == True
             count = ret["data"][0]["count"]
             assert (count >= 0 and count <= 20000)
-            time.sleep(0.5)
+            time.sleep(0.1)
     except Exception as e:
         share_resource["select_exception"] = e
 
-
-threads.append(threading.Thread(target=thread_insert, args= { client1 }))
-threads.append(threading.Thread(target=thread_select, args= { client2 }))
-threads.append(threading.Thread(target=thread_select, args= { client3 }))
-threads.append(threading.Thread(target=thread_select, args= { client4 }))
-threads.append(threading.Thread(target=thread_select, args= { client5 }))
+threads.append(threading.Thread(target=thread_insert, args= { clients[0] }))
+for i in range(1, 30):
+    threads.append(threading.Thread(target=thread_select, args= { clients[i] }))
 
 
 # mock table
 def test_mock_table():
     sql = "create table Student(id varchar(48), name varchar(48), age int, primary key(id));"
-    ret = client1.execute(sql)
+    ret = clients[0].execute(sql)
     assert ret["success"] == True
 
 # test insert with select.
@@ -78,7 +67,5 @@ def test_insert_with_select():
 # drop mock table
 def test_drop_mock_table():
     sql = "drop table Student;"
-    ret = client1.execute(sql)
+    ret = clients[0].execute(sql)
     assert ret["success"] == True
-    client1.close()
-    client2.close()
