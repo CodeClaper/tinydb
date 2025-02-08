0fbab33b31d3f579d1d19128ea721b0007291c8c
zhouchao <zhouchao03@inspur.com>
2025-02-08 14:00:13 +0800

    test: add concurrency query test.

---
+++ test/pytest/concurrency/test_concurrency_select.py
@@ -0,0 +1,43 @@
+from support.tinydb_cli import TinyDbClient
+import threading
+import time
+
+stop = False
+clients = []
+threads = []
+share_resource = {}
+
+for _ in range(0, 30):
+    cli = TinyDbClient("127.0.0.1", 4083)    
+    cli.login("root", "Zc120130211")
+    clients.append(cli)
+
+
+# thread1 to select
+def thread_select(cli):
+    try:
+        for _ in range(1, 60):
+            sql = "select count(1) from bo_qdbixlsx_sheet1;"
+            ret = cli.execute(sql)
+            assert ret["success"] == True
+            count = ret["data"][0]["count"]
+            assert count == 131962
+            time.sleep(0.5)
+            if stop:
+                break
+    except Exception as e:
+        share_resource["select_exception"] = e
+
+for i in range(0, 30):
+    threads.append(threading.Thread(target=thread_select, args= { clients[i] }))
+
+
+# test insert with select.
+def test_concurrency_select():
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+
+    if 'select_exception' in share_resource:
+        raise share_resource['select_exception']
