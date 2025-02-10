e9ddf04a492e963d9d7e5c6df900a4137c252b6b
zhouchao <zhouchao03@inspur.com>
2025-02-10 15:57:08 +0800

    raf: remove comments.

--- src/memory/lomgr.c
+++ src/memory/lomgr.c
@@ -14,7 +14,6 @@
 /* Alloc for local. */
 void *lodalloc(size_t size) {
     void *ptr = MemoryContextAlloc(size);
-    // void *ptr = malloc(size);
     memset(ptr, 0, size);
     return ptr;
 }
@@ -22,13 +21,11 @@ void *lodalloc(size_t size) {
 /* Free for local. */
 void lofree(void *ptr) {
      MemoryContextFree(ptr);
-    //free(ptr);
 }
 
 /* Realloc for local. */
 void *lodrealloc(void *ptr, size_t size) {
     return MemoryContextRealloc(ptr, size);
-    // return realloc(ptr, size);
 }
 
 /* Strdup for local. */
@@ -36,5 +33,4 @@ char *lodstrdup(char *str) {
     if (is_null(str))
         return NULL;
     return MemoryContextStrdup(str);
-    // return strdup(str);
 }
