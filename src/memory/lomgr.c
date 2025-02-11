2c651e30868ffa8a6c7170ca07fb817c4209a073
zhouchao <zhouchao03@inspur.com>
2025-02-11 09:57:26 +0800

    feat: add lock in lomgr to keep sync in muti-threads.

--- src/memory/lomgr.c
+++ src/memory/lomgr.c
@@ -5,16 +5,24 @@
  * Locataion:       src/memory/lodmem.c
  * Description:     This is the implement of mem for local memory.
  *********************************************************************************************/
+#include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "lomgr.h"
 #include "utils.h"
 #include "mctx.h"
 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
 /* Alloc for local. */
 void *lodalloc(size_t size) {
-    void *ptr = MemoryContextAlloc(size);
+    void *ptr;
+    pthread_mutex_lock(&mutex);
+    ptr = MemoryContextAlloc(size);
+    Assert(ptr != NULL);
     memset(ptr, 0, size);
+    pthread_mutex_unlock(&mutex);
     return ptr;
 }
 
@@ -25,12 +33,20 @@ void lofree(void *ptr) {
 
 /* Realloc for local. */
 void *lodrealloc(void *ptr, size_t size) {
-    return MemoryContextRealloc(ptr, size);
+    void *newptr = NULL;
+    pthread_mutex_lock(&mutex);
+    newptr = MemoryContextRealloc(ptr, size);
+    pthread_mutex_unlock(&mutex);
+    return newptr;
 }
 
 /* Strdup for local. */
 char *lodstrdup(char *str) {
+    char *newStr = NULL;
     if (is_null(str))
-        return NULL;
-    return MemoryContextStrdup(str);
+        return newStr;
+    pthread_mutex_lock(&mutex);
+    newStr = MemoryContextStrdup(str);
+    pthread_mutex_unlock(&mutex);
+    return newStr;
 }
